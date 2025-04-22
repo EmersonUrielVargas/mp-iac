c4a9ac226b59620a0ecd1ce22a1218950c864c90
EmersonUrielVargas <emrvargaitaz@gmail.com>
2025-04-22 13:09:31 -0500

    fix format file

--- terraformBackend.tf
+++ terraformBackend.tf
@@ -1,8 +1,8 @@
 
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-mp-001"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
+    bucket = "terraform-state-mp-001"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
   }
 }
